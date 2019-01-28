fda80f24c770f91090c40d8a18e46fa617dec6f1
ANWESHPRATIK <anwesheng@gmail.com>
2019-01-28 12:28:49 +0530

    FirstAndroid

---
+++ app/src/main/java/com/mindweaver/onetoanother/MainActivity.java
@@ -0,0 +1,33 @@
+package com.mindweaver.onetoanother;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn1=(Button)findViewById(R.id.button);
+
+
+         btn1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+                 Intent in=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(in);
+
+             }
+         });
+    }
+}
---
+++ app/src/main/java/com/mindweaver/onetoanother/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.mindweaver.onetoanother;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    Button b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+        b2=(Button)findViewById(R.id.button2);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(in);
+            }
+        });
+
+
+    }
+}
